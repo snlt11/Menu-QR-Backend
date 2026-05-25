e7ad286a0bc5b3d7d8d755ac98b6579cc474489a
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2026-05-25 06:37:52 +0630

    feat: add api.menuqr.asia to central_domains configuration

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,6 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        'api.menuqr.asia',
         '127.0.0.1',
         'localhost',
         'localhost:8000',
