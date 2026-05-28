b8933680ed2ff2ae96eee3d914dc3028e31fbe70
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2026-05-28 15:40:40 +0630

    feat: Update CORS configuration to specify allowed methods, headers, and max age

--- config/cors.php
+++ config/cors.php
@@ -17,20 +17,22 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => [
         'https://menuqr.asia',
         'https://www.menuqr.asia',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
     ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'X-Order-Access-Token'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 86400,
 
     'supports_credentials' => true,
 
